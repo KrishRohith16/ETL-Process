0659a707088d235be0eb561c91550fe8eaf0075b
KrishRohith16 <krishrohith2000@gmail.com>
2023-12-13 20:27:01 +0530

    Created Tables - Emp, City, Dept

--- ETL.sql
+++ ETL.sql
@@ -2,3 +2,28 @@
 CREATE DATABASE ETL;
 USE ETL;
 -- ----------------------------------------------------------------
+
+-- Creating Tables (City, Dept, Emp) :
+CREATE TABLE City(
+city_id INT PRIMARY KEY,
+city_name VARCHAR(50)
+);
+
+CREATE TABLE Dept(
+dept_id INT PRIMARY KEY,
+dept_name VARCHAR(50)
+); 
+
+CREATE TABLE Emp(
+emp_id INT,
+emp_name VARCHAR(50),
+gender ENUM ('M','F'),
+hired_date DATE,
+emp_city_id INT,
+emp_dept_id INT,
+rewards VARCHAR(50),
+monthly_salary VARCHAR(15),
+FOREIGN KEY(emp_city_id) REFERENCES City(city_id),
+FOREIGN KEY(emp_dept_id) REFERENCES Dept(dept_id)
+);
+-- ----------------------------------------------------------------
