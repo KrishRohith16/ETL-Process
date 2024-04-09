4cfdbf061ead43017c1d00fc3ad5c935e5dfce08
KrishRohith16 <krishrohith2000@gmail.com>
2024-04-09 14:35:28 +0530

    Imported CSV Files : Emp, City, & Dept

--- ETL_Process.sql
+++ ETL_Process.sql
@@ -1,3 +1,8 @@
 -- Creating and Utilizing the dtabase 'ETL_Process':
 CREATE DATABASE ETL_PROCESS;
 USE ETL_PROCESS;
+
+-- Imported CSV files into MySQL database.
+-- Emp.csv
+-- City.csv
+-- Dept.csv
